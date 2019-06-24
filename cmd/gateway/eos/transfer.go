e0f3ffb47784c716bff8c652194cd26aafb6d96e
Michael Usher <michael.usher@aarnet.edu.au>
2019-06-24 02:26:41 +0000

    Add accessors so it's a cleaner lock/unlock

--- cmd/gateway/eos/transfer.go
+++ cmd/gateway/eos/transfer.go
@@ -21,5 +21,73 @@ type Transfer struct {
 }
 
 func (mp *Transfer) AddToSize(size int64) {
+	mp.Lock()
 	mp.size += size
+	mp.Unlock()
+}
+
+func (mp *Transfer) IncreasePartsCount() {
+	mp.Lock()
+	mp.partsCount++
+	mp.Unlock()
+}
+
+func (mp *Transfer) AddPart(partid int, part minio.PartInfo) {
+	mp.Lock()
+	mp.parts[partid] = part
+	mp.Unlock()
+}
+
+func (mp *Transfer) GetStagePath() string {
+	mp.RLock()
+	defer mp.RUnlock()
+	return mp.stagepath
+}
+
+func (mp *Transfer) GetSize() int64 {
+	mp.RLock()
+	defer mp.RUnlock()
+	return mp.size
+}
+
+func (mp *Transfer) GetChunkSize() int64 {
+	mp.RLock()
+	defer mp.RUnlock()
+	return mp.chunkSize
+}
+
+func (mp *Transfer) GetFirstByte() byte {
+	mp.RLock()
+	defer mp.RUnlock()
+	return mp.firstByte
+}
+
+func (mp *Transfer) GetContentType() string {
+	mp.RLock()
+	defer mp.RUnlock()
+	return mp.contenttype
+}
+
+func (mp *Transfer) GetMD5() hash.Hash {
+	mp.RLock()
+	defer mp.RUnlock()
+	return mp.md5
+}
+
+func (mp *Transfer) GetMD5PartID() int {
+	mp.RLock()
+	defer mp.RUnlock()
+	return mp.md5PartID
+}
+
+func (mp *Transfer) GetParts() map[int]minio.PartInfo {
+	mp.RLock()
+	defer mp.RUnlock()
+	return mp.parts
+}
+
+func (mp *Transfer) GetPartsCount() int {
+	mp.RLock()
+	defer mp.RUnlock()
+	return mp.partsCount
 }
