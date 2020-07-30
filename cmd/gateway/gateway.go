8d33fd5e94621d0cf46c3edc69f5b01970cfd621
Michael Usher <michael.usher@aarnet.edu.au>
2020-07-30 16:09:41 +1000

    Add missing )

--- cmd/gateway/gateway.go
+++ cmd/gateway/gateway.go
@@ -41,3 +41,4 @@ import (
 	// gateway functionality is frozen, no new gateways are being implemented
 	// or considered for upstream inclusion at this point in time. if needed
 	// please keep a fork of the project.
+)
