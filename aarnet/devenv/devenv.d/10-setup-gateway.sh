75bdc65e1f7473d1043d289c4e4e82f102bb72c7
Michael Usher <michael.usher@aarnet.edu.au>
2021-06-25 14:00:57 +1000

    Fix path to have the correct EOS instance name

--- aarnet/devenv/devenv.d/10-setup-gateway.sh
+++ aarnet/devenv/devenv.d/10-setup-gateway.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-  echo; echo "Creating and setting permissions on minio directory /eos/minioshard/gateways/miniodev"
+  echo; echo "Creating and setting permissions on minio directory /eos/shard/gateways/miniodev"
   echo;
-  quiet_dcexec fst eos -r 0 0 -b mkdir -p /eos/minioshard/gateways/miniodev
-  quiet_dcexec fst eos -r 0 0 -b chown 48:48 /eos/minioshard/gateways/miniodev
-  quiet_dcexec mgm eos -r 0 0 -b attr -r set sys.forced.checksum=md5 /eos/minioshard/gateways/miniodev
+  quiet_dcexec fst eos -r 0 0 -b mkdir -p /eos/shard/gateways/miniodev
+  quiet_dcexec fst eos -r 0 0 -b chown 48:48 /eos/shard/gateways/miniodev
+  quiet_dcexec mgm eos -r 0 0 -b attr -r set sys.forced.checksum=md5 /eos/shard/gateways/miniodev
