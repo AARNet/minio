001faef59074dada5e1de0187aacecbd0948ffdf
Michael Usher <michael.usher@aarnet.edu.au>
2021-06-25 16:29:26 +1000

    Fix path being outputted

--- aarnet/devenv/devenv.d/10-setup-gateway.sh
+++ aarnet/devenv/devenv.d/10-setup-gateway.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-  echo; echo "Creating and setting permissions on minio directory /eos/shard/gateways/miniodev"
+  echo; echo "Creating and setting permissions on minio directory /eos/shard/data/gateways/miniodev"
   echo;
   quiet_dcexec fst eos -r 0 0 -b mkdir -p /eos/shard/data/gateways/miniodev
   quiet_dcexec fst eos -r 0 0 -b chown 48:48 /eos/shard/data/gateways/miniodev
