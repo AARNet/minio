c632be64cf23bf2034ff0190ed6fb175ffb9392c
Michael Usher <michael.usher@aarnet.edu.au>
2021-07-05 10:19:10 +1000

    CSOPS-404: Move configuration required for the filesystem to methods so that we can re-use them

--- cmd/gateway/eos/eos.go
+++ cmd/gateway/eos/eos.go
@@ -38,29 +38,12 @@ func (g *EOS) Name() string {
 
 // NewGatewayLayer returns eos gatewaylayer.
 func (g *EOS) NewGatewayLayer(creds auth.Credentials) (minio.ObjectLayer, error) {
-	// TODO: Move loglevel to something other than eosObjects
-	loglevel, ok := strconv.Atoi(os.Getenv("EOSLOGLEVEL"))
-	if ok != nil {
-		MaxLogLevel = LogLevelOff
-	} else {
-		MaxLogLevel = loglevel
-	}
+
+	MaxLogLevel = g.GetLogLevel()
 
 	stage := os.Getenv("EOSSTAGE")
 	if stage != "" {
 		os.MkdirAll(stage, 0700)
-	}
-
-	readonly := false
-	if os.Getenv("EOSREADONLY") == "true" {
-		readonly = true
-	}
-
-	readmethod := readMethodWebdav
-	if strings.ToLower(os.Getenv("EOSREADMETHOD")) == readMethodXrootd {
-		readmethod = readMethodXrootd
-	} else if strings.ToLower(os.Getenv("EOSREADMETHOD")) == readMethodXrdcp {
-		readmethod = readMethodXrdcp
 	}
 
 	validbuckets := true
@@ -74,7 +57,7 @@ func (g *EOS) NewGatewayLayer(creds auth.Credentials) (minio.ObjectLayer, error)
 	}
 	eosLogger.Startup("EOS staging: %s", stage)
 
-	if readonly {
+	if g.IsReadOnly() {
 		eosLogger.Startup("EOS read only mode: ENABLED")
 	}
 
@@ -82,42 +65,103 @@ func (g *EOS) NewGatewayLayer(creds auth.Credentials) (minio.ObjectLayer, error)
 		eosLogger.Startup("EOS allowing invalid bucket names (RISK)")
 	}
 
-	httphost := os.Getenv("EOSHTTPHOST")
-	if httphost == "" {
-		httphost = os.Getenv("EOS") + ":8000"
-	}
-
-	maxRetry, ok := strconv.Atoi(os.Getenv("MAX_RETRY"))
-	if ok != nil {
-		maxRetry = 10
-	}
-
 	maxKeys, ok := strconv.Atoi(os.Getenv("OVERWRITEMAXKEYS"))
 	if ok != nil {
 		maxKeys = 0
 	}
 
-	sort, ok := strconv.Atoi(os.Getenv("EOSSORTFILELISTING"))
-	if ok != nil {
-		sort = 0
-	}
-
 	eosLogger.Startup("EOS URL: %s", os.Getenv("EOS"))
-	eosLogger.Startup("EOS HTTP URL: %s", httphost)
+	eosLogger.Startup("EOS HTTP URL: %s", g.GetHTTPHost())
 	eosLogger.Startup("EOS HTTP Proxy: %s", os.Getenv("EOS_HTTP_PROXY"))
 	eosLogger.Startup("EOS VOLUME PATH: %s", os.Getenv("VOLUME_PATH"))
 	eosLogger.Startup("EOS USER (uid:gid): %s (%s:%s)", os.Getenv("EOSUSER"), os.Getenv("EOSUID"), os.Getenv("EOSGID"))
 	eosLogger.Startup("EOS file hooks url: %s", os.Getenv("HOOKSURL"))
 	eosLogger.Startup("EOS SCRIPTS PATH: %s", os.Getenv("SCRIPTS"))
-	eosLogger.Startup("EOS READ METHOD: %s", readmethod)
-	eosLogger.Startup("EOS MAX RETRY: %d", maxRetry)
+	eosLogger.Startup("EOS READ METHOD: %s", g.GetReadMethod())
+	eosLogger.Startup("EOS MAX RETRY: %d", g.GetMaxRetry())
 	eosLogger.Startup("EOS OVERWRITE MAX KEYS: %d", maxKeys)
-	eosLogger.Startup("EOS SORT FILE LISTING: %d", sort)
-	eosLogger.Startup("EOS LOG LEVEL: %d", loglevel)
+	eosLogger.Startup("EOS SORT FILE LISTING: %t", g.GetSort())
+	eosLogger.Startup("EOS LOG LEVEL: %d", MaxLogLevel)
 
-	// Init filesystem
+	// and go
+	return &eosObjects{
+		maxRetry:     g.GetMaxRetry(),
+		maxKeys:      maxKeys,
+		path:         os.Getenv("VOLUME_PATH"),
+		hookurl:      os.Getenv("HOOKSURL"),
+		stage:        stage,
+		validbuckets: validbuckets,
+		TransferList: NewTransferList(),
+		FileSystem:   g.SetupFileSystem(),
+		readonly:     g.IsReadOnly(),
+	}, nil
+}
+
+// Production - eos gateway is production ready.
+func (g *EOS) Production() bool {
+	return false
+}
+
+// GetLogLevel - returns the configured log level
+func (g *EOS) GetLogLevel() int {
+	loglevel, ok := strconv.Atoi(os.Getenv("EOSLOGLEVEL"))
+	if ok != nil {
+		loglevel = LogLevelOff
+	}
+	return loglevel
+}
+
+// GetReadMethod - returns the configured read method
+func (g *EOS) GetReadMethod() string {
+	readmethod := readMethodWebdav
+	if strings.ToLower(os.Getenv("EOSREADMETHOD")) == readMethodXrootd {
+		readmethod = readMethodXrootd
+	} else if strings.ToLower(os.Getenv("EOSREADMETHOD")) == readMethodXrdcp {
+		readmethod = readMethodXrdcp
+	}
+	return readmethod
+}
+
+// GetHTTPHost - return the host configured by the environment
+func (g *EOS) GetHTTPHost() string {
+	httphost := os.Getenv("EOSHTTPHOST")
+	if httphost == "" {
+		httphost = os.Getenv("EOS") + ":8000"
+	}
+	return httphost
+}
+
+// GetMaxRetry - return the max number of retries as configured by the environment
+func (g *EOS) GetMaxRetry() int {
+	maxRetry, ok := strconv.Atoi(os.Getenv("MAX_RETRY"))
+	if ok != nil {
+		maxRetry = 10
+	}
+	return maxRetry
+}
+
+// IsReadOnly - return whether the gateway has been configured as read only
+func (g *EOS) IsReadOnly() bool {
+	readonly := false
+	if os.Getenv("EOSREADONLY") == "true" {
+		readonly = true
+	}
+	return readonly
+}
+
+// GetSort - return whether or not to sort the file listing
+func (g *EOS) GetSort() bool {
+	sort, ok := strconv.Atoi(os.Getenv("EOSSORTFILELISTING"))
+	if ok != nil {
+		sort = 0
+	}
+	return (sort > 0)
+}
+
+// SetupFileSystem - configures the filesystem
+func (g *EOS) SetupFileSystem() *EOSFS {
 	xrdcp := &Xrdcp{
-		maxRetry: maxRetry,
+		MaxRetry: g.GetMaxRetry(),
 		MGMHost:  os.Getenv("EOS"),
 		Path:     os.Getenv("VOLUME_PATH"),
 		User:     os.Getenv("EOSUSER"),
@@ -125,37 +169,21 @@ func (g *EOS) NewGatewayLayer(creds auth.Credentials) (minio.ObjectLayer, error)
 		GID:      os.Getenv("EOSGID"),
 	}
 
-	filesystem := &eosFS{
-		maxRetry:   maxRetry,
-		sort:       (sort > 0),
+	filesystem := &EOSFS{
+		MaxRetry:   g.GetMaxRetry(),
+		Sort:       g.GetSort(),
 		MGMHost:    os.Getenv("EOS"),
-		HTTPHost:   httphost,
+		HTTPHost:   g.GetHTTPHost(),
 		Proxy:      os.Getenv("EOS_HTTP_PROXY"),
 		Path:       os.Getenv("VOLUME_PATH"),
 		User:       os.Getenv("EOSUSER"),
 		UID:        os.Getenv("EOSUID"),
 		GID:        os.Getenv("EOSGID"),
-		ReadMethod: readmethod,
+		ReadMethod: g.GetReadMethod(),
 		Scripts:    os.Getenv("SCRIPTS"),
 		StatCache:  NewRequestStatCache(os.Getenv("VOLUME_PATH")),
 		Xrdcp:      xrdcp,
 	}
 
-	// and go
-	return &eosObjects{
-		maxRetry:     maxRetry,
-		maxKeys:      maxKeys,
-		path:         os.Getenv("VOLUME_PATH"),
-		hookurl:      os.Getenv("HOOKSURL"),
-		stage:        stage,
-		validbuckets: validbuckets,
-		TransferList: NewTransferList(),
-		FileSystem:   filesystem,
-		readonly:     readonly,
-	}, nil
-}
-
-// Production - eos gateway is production ready.
-func (g *EOS) Production() bool {
-	return false
+	return filesystem
 }
